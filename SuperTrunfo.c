1455c86138ff352babdd7db8ee8dce8743546aef
CustodioDoug <douglas.custodio289@gmail.com>
2025-03-12 17:00:35 -0300

    DesafioSuperTrunfo

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main(){
     char Carta1 = '1';
     char Estado1 = 'A';
@@ -9,6 +8,8 @@ int main(){
     float Area1 = 152120;
     float PIB1 = 699.0;
     int PontosTuristicos1 = 36;
+    float DensidadePop1 = NomeDaCidade1 / Area1;
+    float PibPC1 = PIB1 / Populacao1;
 
     char Carta2 = '2';
     char Estado2 = 'B';
@@ -18,6 +19,9 @@ int main(){
     float Area2 = 120000;
     float PIB2 = 671.0;
     int PontosTuristicos2 = 28;
+    float DensidadePop2 = NomeDaCidade2 / Area2;
+    float PibPC2 = PIB2 / Populacao2;
+
 
     printf("Carta1 \n");
     printf("Estado: %c\n", Estado1);
